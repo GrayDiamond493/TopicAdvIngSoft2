6d1fa815ab3e56f6bf6aaa02a40e3ce40c1ebfd5
damesa <damesa@bancolombia.com.co>
2024-10-17 19:56:00 -0500

    improve any xpaths used in the test

--- pages/PokemonDataPage.ts
+++ pages/PokemonDataPage.ts
@@ -4,52 +4,50 @@ export class PokemonDataPage {
   constructor(private page: Page) {}
 
   async setItem(item: string) {
-    await this.page.fill('//input[@name="item"]', item);
+    await this.page.fill('input[name="item"]', item);
   }
 
   async setNickname(nickname: string) {
-    await this.page.fill('//input[@name="nickname"]', nickname);
+    await this.page.fill('input[name="nickname"]', nickname);
   }
 
-  async setDetails(details: {level: string; gender: string; shiny: string}) {
-    await this.page.click('//button[@name="details"]')
-    await this.page.fill('//input[@name="level"]', details.level);
-    const genderSelector = `//input[@type="radio" and @name="gender" and @value="${details.gender}"]`
-    const isGenderSelectable = await this.page.locator(genderSelector).isVisible();
+  async setDetails(details: { level: string; gender: string; shiny: string }) {
+    await this.page.click('button[name="details"]');
+    await this.page.fill('input[name="level"]', details.level);
 
-  if (isGenderSelectable) {
-    await this.page.click(genderSelector);
-  } else {
-    console.log(`Pokémon has fixed gender`);
-  }
+    const genderRadio = this.page.locator(`input[type="radio"][name="gender"][value="${details.gender}"]`);
+    if (await genderRadio.isVisible()) {
+      await genderRadio.click();
+    } else {
+      console.log(`Pokémon has fixed gender`);
+    }
 
-    
-  await this.page.click(`//input[@type="radio" and @name="shiny" and @value="${details.shiny}"]`);
+    await this.page.locator(`input[type="radio"][name="shiny"][value="${details.shiny}"]`).click();
   }
 
   async setMoves(moves: { move1: string; move2: string; move3: string; move4: string }) {
-    await this.page.fill('//input[@name="move1"]', moves.move1);
-    await this.page.fill('//input[@name="move2"]', moves.move2);
-    await this.page.fill('//input[@name="move3"]', moves.move3);
-    await this.page.fill('//input[@name="move4"]', moves.move4);
+    await this.page.fill('input[name="move1"]', moves.move1);
+    await this.page.fill('input[name="move2"]', moves.move2);
+    await this.page.fill('input[name="move3"]', moves.move3);
+    await this.page.fill('input[name="move4"]', moves.move4);
   }
 
   async setEVs(evs: { hp: string; atk: string; def: string; spa: string; spd: string; spe: string }) {
-    await this.page.click('//button[@name="stats"]')
-    await this.page.fill('//input[@name="evslider-hp"]', evs.hp);
-    await this.page.fill('//input[@name="evslider-atk"]', evs.atk);
-    await this.page.fill('//input[@name="evslider-def"]', evs.def);
-    await this.page.fill('//input[@name="evslider-spa"]', evs.spa);
-    await this.page.fill('//input[@name="evslider-spd"]', evs.spd);
-    await this.page.fill('//input[@name="evslider-spe"]', evs.spe);
+    await this.page.click('button[name="stats"]');
+    await this.page.fill('input[name="evslider-hp"]', evs.hp);
+    await this.page.fill('input[name="evslider-atk"]', evs.atk);
+    await this.page.fill('input[name="evslider-def"]', evs.def);
+    await this.page.fill('input[name="evslider-spa"]', evs.spa);
+    await this.page.fill('input[name="evslider-spd"]', evs.spd);
+    await this.page.fill('input[name="evslider-spe"]', evs.spe);
   }
 
   async verifyTotalEvs() {
-    const remainingEVs = await this.page.textContent('//div[@class="totalev"]//em');
+    const remainingEVs = await this.page.textContent('div.totalev em');
     expect(remainingEVs).toEqual("0");
   }
 
   async goBackToTeam() {
-    await this.page.click('//button[@name="back"]'); 
+    await this.page.click('button[name="back"]');
   }
 }
